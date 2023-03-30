2080ea6fdcb217661514818e6a4bd12eb5ffb087
thingsees <127774739+thingsees@users.noreply.github.com>
2023-03-30 18:28:10 -0300

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,14 @@
+  #include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,13,4,3,5);
+void setup() {
+  // put your setup code here, to run once:
+   lcd.begin(16,2);
+   lcd.print("HEINZ");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   lcd.setCursor(0,1);
+   lcd.print(millis()/1000);
+}

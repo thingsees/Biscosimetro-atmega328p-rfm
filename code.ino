8c53305dcb4451d7fb55a3c77dac10a2ef7f65b9
thingsees <127774739+thingsees@users.noreply.github.com>
2023-03-31 02:32:32 -0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -3,6 +3,7 @@
 LiquidCrystal lcd(12,11,13,4,3,5);
 void setup() {
   // put your setup code here, to run once:
+  // el pin 2 se utiliza para el sensor magnetico
    lcd.begin(16,2);
    lcd.print("HEINZ");
 }
